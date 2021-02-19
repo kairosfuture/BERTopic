cd42c5634e30ca44534c560e8f82d664a92c1774
dopc <aliacar23@gmail.com>
2021-02-19 22:45:43 +0300

    umap seed added

--- bertopic/_bertopic.py
+++ bertopic/_bertopic.py
@@ -747,7 +747,8 @@ class BERTopic:
             self.umap_model = umap.UMAP(n_neighbors=self.n_neighbors,
                                         n_components=self.n_components,
                                         min_dist=0.0,
-                                        metric='cosine').fit(embeddings)
+                                        metric='cosine',
+                                        random_state=42).fit(embeddings)
         umap_embeddings = self.umap_model.transform(embeddings)
         logger.info("Reduced dimensionality with UMAP")
         return umap_embeddings
